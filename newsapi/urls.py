e568e36222860a0fe4acec143f53a50b0bd2bff8
lixiangsanxun <534551900@qq.com>
2018-10-12 18:40:16 +0800

    路由添加

--- newsapi/urls.py
+++ newsapi/urls.py
@@ -14,8 +14,40 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
+import article.views as views
+from rest_framework.routers import DefaultRouter
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='新闻系统 API')
+
+# 建立一个路由器对象
+router = DefaultRouter()
+
+# 将我们的路由注册到url里
+router.register(r'category', views.CategoryViewset, base_name="category")
+router.register(r'categoryitems', views.CategoryitemsViewset, base_name="categoryitems")
+router.register(r'item', views.ItemViewset, base_name="item")
+
+router.register(r'categoryStringitems', views.CategoryStringitemsViewset, base_name="categoryStringitems")
+router.register(r'categoryPrimaryKeyitems', views.CategoryPrimaryKeyitemsViewset, base_name="categoryPrimaryKeyitems")
+router.register(r'categorySlugitems', views.CategorySlugitemsViewset, base_name="categorySlugitems")
+
+router.register(r'tag', views.TagViewset, base_name="tag")
+router.register(r'ad', views.AdViewset, base_name="ad")
+
+router.register(r'articleList', views.ArticleListViewSet, base_name="articleList")
+router.register(r'hot_articleList', views.Hot_articleListViewSet, base_name="hot_articleList")
+
+router.register(r'user', views.UserViewset, base_name="user")
+router.register(r'userFav', views.UserFavViewset, base_name="userFav")
+router.register(r'userLogin', views.UserLoginViewset, base_name="userLogin")
+router.register(r'setPassword', views.UserSetPasswordViewset, base_name="setPassword")
+
 
 urlpatterns = [
+    re_path('^$', schema_view),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
